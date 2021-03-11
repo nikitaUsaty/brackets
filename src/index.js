331bf5397c4500b36a9285c77f7cab84c2f94df1
nikitausaty <nikitausatiy@yandex.ru>
2021-03-12 02:58:38 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let brc = {
+        "}": "{",
+        "]": "[",
+        ")": "(",
+    };
+
+    let st = [];
+    for (let i = 0; i < str.length; i++) {
+        if (Object.values(brc).includes(str[i])) {
+            st.push(str[i]);
+        } else {
+            if (brc[str[i]] !== st.pop()) {
+                return false;
+            }
+        }
+    }
+    return st.length === 0;
+};
